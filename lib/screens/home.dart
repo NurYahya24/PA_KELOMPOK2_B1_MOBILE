63cf9f551c2bee2de3e477b46dd738a4c9b8ce38
Dhea Puspita <dheapuspita363@gmail.com>
2023-11-17 15:12:16 +0800

    journal page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'journal.dart';
 import 'affirmation.dart';
@@ -14,30 +15,41 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int _index = 0;
   void _onItemTap(int index) {
-    setState(() {
-      _index = index;
-    });
+    setState(
+      () {
+        _index = index;
+      },
+    );
   }
 
   static List<Widget> _pages = [
-    journal_page(),
+    JournalPage(),
     affirmation_page(),
     quotes_page(),
     highlight_page(),
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: GestureDetector(
           onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return const Profile_page();
-            }));
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return const Profile_page();
+                },
+              ),
+            );
           },
-          child: CircleAvatar(
-            radius: 35,
-            child: Icon(Icons.person),
+          child: Padding(
+            padding: EdgeInsets.only(top: 15, left: 25),
+            child: CircleAvatar(
+              radius: 60,
+              child: Icon(Icons.person),
+            ),
           ),
         ),
       ),
@@ -45,23 +57,34 @@ class _HomeState extends State<Home> {
         child: _pages.elementAt(_index),
       ),
       bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          // backgroundColor: Color.fromARGB(255, 253, 233, 235),
-          // unselectedItemColor: Color.fromARGB(255, 221, 218, 219),
-          // selectedItemColor: const Color.fromARGB(255, 229, 69, 107),
-          backgroundColor: Color.fromARGB(255, 255, 226, 226),
-          unselectedItemColor: Color.fromARGB(255, 170, 170, 170),
-          selectedItemColor: const Color.fromARGB(255, 234, 138, 138),
-          currentIndex: _index,
-          onTap: _onItemTap,
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.book), label: "Journal"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.heat_pump_rounded), label: "Affirmation"),
-            BottomNavigationBarItem(icon: Icon(Icons.circle), label: "Quotes"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.image), label: 'Highlight'),
-          ]),
+        type: BottomNavigationBarType.fixed,
+        // backgroundColor: Color.fromARGB(255, 253, 233, 235),
+        // unselectedItemColor: Color.fromARGB(255, 221, 218, 219),
+        // selectedItemColor: const Color.fromARGB(255, 229, 69, 107),
+        backgroundColor: Color.fromARGB(255, 255, 226, 226),
+        unselectedItemColor: Color.fromARGB(255, 170, 170, 170),
+        selectedItemColor: const Color.fromRGBO(224, 46, 129, 1),
+        currentIndex: _index,
+        onTap: _onItemTap,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.book),
+            label: 'Journal',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.heart),
+            label: 'Affirmation',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.circle),
+            label: 'Quotes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.square),
+            label: 'Highlight',
+          ),
+        ],
+      ),
     );
   }
 }
