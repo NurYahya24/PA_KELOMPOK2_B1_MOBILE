fb075f3e8181907d53e2accf4ef880ae70e8274b
zahrasalsabila12 <zah12asalsabila@gmail.com>
2023-12-01 18:59:08 +0800

    section page

--- lib/screens/section.dart
+++ lib/screens/section.dart
@@ -1,8 +1,31 @@
 import 'package:daily_jurnal/screens/highlight.dart';
 import 'package:flutter/material.dart';
 
-class section_page extends StatelessWidget {
+class section_page extends StatefulWidget {
   const section_page({super.key});
+
+  @override
+  State<section_page> createState() => _section_pageState();
+}
+
+class _section_pageState extends State<section_page> {
+  bool isSelected = false;
+  TextEditingController _textController = TextEditingController();
+  Color _buttonColor = Color(0xffDDDDDD);
+  Color _textColor = Colors.black;
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  void _onTextFieldChanged(String value) {
+    setState(() {
+      _buttonColor = value.isEmpty ? Color(0xffDDDDDD) : Color(0xFFFF8787);
+      _textColor = value.isEmpty ? Colors.black : Colors.white;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,9 +38,11 @@ class section_page extends StatelessWidget {
             child: IconButton(
               onPressed: () {
                 Navigator.pop(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const highlight_page()));
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const highlight_page(),
+                  ),
+                );
               },
               icon: Icon(Icons.arrow_back_rounded),
               iconSize: 30,
@@ -45,8 +70,10 @@ class section_page extends StatelessWidget {
             ),
           ),
           Container(
-            margin: EdgeInsets.only(left: 24, right: 24, bottom: 10),
+            margin: EdgeInsets.only(left: 24, right: 24, bottom: 350),
             child: TextField(
+              controller: _textController,
+              onChanged: _onTextFieldChanged,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 hintText: 'Name your section',
@@ -54,8 +81,31 @@ class section_page extends StatelessWidget {
                   borderSide: BorderSide(color: Colors.black),
                 ),
                 focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                      color: Color(0xFFFF8787)),
+                  borderSide: BorderSide(color: Color(0xFFFF8787)),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 24),
+            alignment: Alignment.bottomCenter,
+            child: SizedBox(
+              width: double.infinity, 
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    "Continue",
+                    style: TextStyle(color: _textColor),
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: _buttonColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                        30),
+                  ),
                 ),
               ),
             ),
